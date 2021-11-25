0b2137b49b377c7545bad80a4f466048fd4780fc
sunshiyu <sunshiyu@shulie.io>
2021-11-25 15:02:08 +0800

    optimize support query global config

--- amdb-app/src/main/java/io/shulie/amdb/service/impl/TroDataServiceImpl.java
+++ amdb-app/src/main/java/io/shulie/amdb/service/impl/TroDataServiceImpl.java
@@ -3,6 +3,8 @@ package io.shulie.amdb.service.impl;
 import io.shulie.amdb.common.request.trodata.TrodataQueryParam;
 import io.shulie.amdb.service.TroDataService;
 import io.shulie.amdb.tro.mapper.TroDataMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +14,20 @@ import org.springframework.stereotype.Service;
  * @datetime 2021-09-27 7:45 下午
  */
 @Service
+@Slf4j
 public class TroDataServiceImpl implements TroDataService {
     @Autowired
     TroDataMapper troDataMapper;
 
     @Override
     public String queryTroData(TrodataQueryParam param) {
-        return troDataMapper.queryConfigValueByParams(param.getUserAppKey(), param.getEnvCode(), param.getAppName(), param.getConfigKey());
+        //根据租户
+        String samplingIntervel = troDataMapper.queryConfigValueByParams(param.getUserAppKey(), param.getEnvCode(), param.getAppName(), param.getConfigKey());
+        log.info("query app samplingInterval params:{},result:{}", param, samplingIntervel);
+        if (StringUtils.isBlank(samplingIntervel)) {
+            //查询全局配置
+            return troDataMapper.queryDefaultConfigValueByParams(param.getConfigKey());
+        }
+        return samplingIntervel;
     }
 }
