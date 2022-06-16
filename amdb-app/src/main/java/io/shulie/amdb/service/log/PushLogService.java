aa856a3566bd0477102fc70560357b62ace85787
kither <wuchunjing@shulie.io>
2022-06-16 14:42:39 +0800

    1、链路查询接口增加是否压测字段 2、数据校准增加重复性校验

--- amdb-app/src/main/java/io/shulie/amdb/service/log/PushLogService.java
+++ amdb-app/src/main/java/io/shulie/amdb/service/log/PushLogService.java
@@ -27,10 +27,11 @@ public interface PushLogService {
     /**
      * 向AMDB推送数据
      *
-     * @param data    数据
+     * @param filePath 文件路径
+     * @param traceLine    数据
      * @param version 版本
      */
-    void pushLogToAmdb(byte[] data, String version, String address) throws Exception;
+    void pushLogToAmdb(String filePath, String traceLine, String version, String address);
 
 
     void callbackTakin(String callbackUrl, LogCompensateCallbackRequest request);
