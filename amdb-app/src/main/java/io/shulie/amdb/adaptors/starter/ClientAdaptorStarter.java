e389137c3e58151b78585f0ee0fe73babd435c59
xiaojianzhang <xiaojianzhang@shulie.io>
2022-11-24 16:34:28 +0800

    fix:启动amdb不删表数据

--- amdb-app/src/main/java/io/shulie/amdb/adaptors/starter/ClientAdaptorStarter.java
+++ amdb-app/src/main/java/io/shulie/amdb/adaptors/starter/ClientAdaptorStarter.java
@@ -58,6 +58,6 @@ public class ClientAdaptorStarter extends DefaultFactoryAdaptorStarter {
         /**
          * turn cate agent config
          */
-        agentConfigService.truncateTable();
+//        agentConfigService.truncateTable();
     }
 }
